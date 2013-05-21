3d52b00f238512de1c1e91301a465e951302bd00
Zack Rusin <zack@kde.org>
2013-05-20 23:16:21 -0400

    Use js2-mode by default

--- emacs/dotemacs-js.el
+++ emacs/dotemacs-js.el
@@ -1,13 +1,10 @@
+(add-to-list 'load-path (concat sdk-directory "emacs/libs/js2-mode"))
+(require 'js2-mode)
+
 ;(add-to-list 'grep-files-aliases (cons "js" "*.js *.json"))
 
-(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
 
-(add-hook 'js-mode-hook
-          '(lambda ()
-             (setq js-indent-level 4)
-             (setq indent-tabs-mode nil)
-             (setq js-auto-indent-flag nil)
-             ))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
 (provide 'dotemacs-js)
